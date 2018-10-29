b96f994218e5cddb1039181205fc42a88b8835ec
PC-STUDIO\sara <s.locatelli17@studenti.unibg.it>
2018-10-29 21:09:59 +0100

    edr extracted

--- prova.m
+++ prova.m
@@ -1,7 +1,22 @@
-addpath('C:\Users\sara\Downloads\pam_tompkin')
 
-filename='apnea-ecg/a01'
-f='a01'
-[signal,Fs,tm]=rdsamp(filename);
-[qrs_amp_raw,qrs_i_raw,delay]=pan_tompkin(signal(1:1000),Fs,1)
-%plot(qrs_i_raw,qrs_amp_raw)
+
+signal='apnea-ecg/a01';
+r_peaks='qrs';
+data_type=1;
+channel=2;
+show=1;
+y=edr(data_type,signal,r_peaks,[],[],[],channel,show);
+wfdb2mat('a01')
+[~,signal,Fs,~]=rdmat('a01m');
+resp=signal(:,1);
+resp=resp-mean(resp);
+resp=resp*200;
+sec=length(resp)/Fs;
+xax=[.25:.25:sec];
+r=interp1(y(:,1), y(:,2), xax, 'spline');
+figure
+plot(xax,r)
+hold on
+plot([1:length(resp)]/Fs,resp,'r')
+legend('edr','respiratory signal')
+xlabel('time (s)')
